7dc078d43e22568f8ea55324e696b068cd2169a2
pedro_espigares <pedritoesp04@gmail.com>
2023-02-27 13:12:53 +0100

    Prueba para deploy

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const http = require('http');
-const server = http.createServer(app);
+const port = 443;
+const https = require('https');
+const server = https.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const { writeFile, unlink, existsSync } = require("fs");
